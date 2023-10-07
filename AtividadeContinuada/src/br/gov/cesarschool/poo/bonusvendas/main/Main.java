15f7d8473d09ec2e0d005d0defebd787154970e6
pedro-coelho-dr <pcdr@cesar.school>
2023-10-07 11:46:41 -0300

    Create Main.java

---
+++ AtividadeContinuada/src/br/gov/cesarschool/poo/bonusvendas/main/Main.java
@@ -0,0 +1,15 @@
+package br.gov.cesarschool.poo.bonusvendas.main;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
