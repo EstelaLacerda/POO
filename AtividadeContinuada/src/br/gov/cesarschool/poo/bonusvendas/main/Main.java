608401632e115be6cab74bddbc9cc8fe77a8df61
Caio <hiratelli.0@gmail.com>
2023-10-07 14:17:37 -0300

    teste de main

--- AtividadeContinuada/src/br/gov/cesarschool/poo/bonusvendas/main/Main.java
+++ AtividadeContinuada/src/br/gov/cesarschool/poo/bonusvendas/main/Main.java
@@ -1,5 +1,11 @@
 package br.gov.cesarschool.poo.bonusvendas.main;
 
+import java.time.LocalDate;
+
+import br.gov.cesarschool.poo.bonusvendas.entidade.Vendedor;
+import br.gov.cesarschool.poo.bonusvendas.entidade.geral.Endereco;
+import br.gov.cesarschool.poo.bonusvendas.entidade.geral.Sexo;
+
 public class Main {
 
 	public Main() {
@@ -9,7 +15,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		LocalDate dtN = LocalDate.of(1990, 5, 15);
+		Endereco ender = new Endereco("rua barbie", 6969, "tome", "55666", "recife", "pernambuco",
+			"braziu");
+		
+		Vendedor v1 = new Vendedor("12345", "caio", Sexo.FEMININO, dtN, 500.0, ender);
+		
+		System.out.println(v1.calcularIdade());
+		
+		
 	}
 
 }
