6099e46d0b255f151d8fdebd8c2ff29fcf95cf0c
Caio <hiratelli.0@gmail.com>
2023-11-12 10:43:32 -0300

    teste de interface

---
+++ bonusvendas/src/br/gov/cesarschool/poo/bonusvendas/entidade/geral/Registro.java
@@ -0,0 +1,9 @@
+package br.gov.cesarschool.poo.bonusvendas.entidade.geral;
+
+import java.io.Serializable;
+
+public abstract class Registro implements Serializable{
+	
+	
+	public abstract String getIdUnico();
+}
